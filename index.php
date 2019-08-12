a1c81f6b1ab249a56920eb5562a390c3d7733bab
manizkou <alertmanishrana@gmail.com>
2019-08-12 12:50:13 +0545

    index.css and index.php changes

--- index.php
+++ index.php
@@ -1,30 +1,12 @@
-<?php
-ob_start();
-include('../classes/application_top.php');
-if(empty($_SESSION['admin'])){
-	header("Location:adminLogin.php");
-	}
-if(!empty($_REQUEST['pg'])){
-	$page=$_REQUEST['pg'];
-	
-?>	
-
-
 	<?php
 		include('header.php');
 	?>
 
-	<section>
+<!-- 	<section>
 		
-	</section>
+	</section> -->
 	
 
 	<?php
 		include('footer.php');
 	?>
-<?php
-	}
-?>
-<?php
-ob_end_flush();
- ?>
