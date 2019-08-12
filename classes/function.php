cfd714ecad9424e4d881d281a9fe664441c88509
drainthapa <drainthapa@gmail.com>
2019-08-12 16:27:02 +0545

    donar register updated

--- classes/function.php
+++ classes/function.php
@@ -20,7 +20,37 @@ class Functions extends DataBase{
 		}
 		
 	}
+	function DonorRegister($name,$bloodgroup,$contact,$landlinecontact,$city,$address,$email,$availability,$i_name){
 
+		$sql="insert into tbl_donarreg(name,bloodgroup,contact,landlinecontact,city,address,email,availability,image) values('$name','$bloodgroup','$contact','$landlinecontact','$city','$address','$email','$availability','$i_name')";
+		$reg=$this->exec($sql);
+		// if($reg){
+		// 	$this->redirect('./dashboard.php?pg='.$p.'&smg=Operation Successful');
+		// }else{
+		// 	$this->error("Error Insert");
+		// }
+	}
+	function UploadFile($i_name,$i_size,$i_type,$i_temp,$d){
+
+		if($i_type=='image/jpeg' || $i_type=='image/png' || $i_type=='image/gif'){
+
+			if($i_type<=8388608){
+				$up=move_uploaded_file($i_temp,$i_name);
+				if($up){
+					return 1;
+				}
+			}else{
+				return "Image Size very Large !";
+			}
+
+		}else{
+			return "Invalid Image Type";
+		}
+
+
+
+
+	}
 }
 
 ?>
--- donorregister.php
+++ donorregister.php
@@ -1,106 +1,122 @@
 <?php
-	include('header.php');
+require_once('classes/application_top.php');
+if(isset($_POST['submit'])){
+	$name=$_POST['name'];
+	$bloodgroup=$_POST['bloodgroup'];
+	$contact=$_POST['contact'];
+	$landlinecontact=$_POST['landlinecontact'];
+	$city=$_POST['city'];
+	$address=$_POST['address'];
+	$email=$_POST['email'];
+	$availability=$_POST['availability'];
+	
+	//Image
+	$i_name=$_FILES['image']['name'];
+	$i_size=$_FILES['image']['size'];
+	$i_type=$_FILES['image']['type'];
+	$i_tmp=$_FILES['image']['tmp_name'];
+
+// Data
+	
+	$fupload=$obj->UploadFile($i_name,$i_size,$i_type,$i_tmp,'donarimg');
+	if($fupload==1){
+		$register=$obj->DonorRegister($name,$bloodgroup,$contact,$landlinecontact,$city,$address,$email,$availability,$i_name);
+	}else{
+		echo $fupload;
+	}
+}
+?>
+
+
+
+<?php
+include('header.php');
 ?>
 <div class="container">
 	
-		<div class="row">
-			<div class="col-sm-8 offset-sm-2 col-md-6 offset-md-3 ">
-				<div class="donorForm">
-					<div class="submit-form text-center">
-						<h4>Blood Donors Register</h4>
-						<p>
-							Please fill the following information to register donor.
-						</p>
-						<hr>
-					</div>
-					<form action="/" method="POST" enctype="multipart/form-data">
-						<div class="form-group">
-							<label for="fullName">Full name*</label>
-							<input type="text" class="form-control" id="fullName" aria-describedby="fullName" placeholder="Enter full name">
-						</div>
-
-						<div class="form-group">
-							<label for="bloodGroup">Blood Group*</label>
-							<select class="form-control" id="bloodGroup">
-								<option selected="selected" disabled="disabled">-----Select-----</option>
-								<option value="A+">A+</option>
-								<option value="A-">A-</option>
-								<option value="B+">B+</option>
-								<option value="B-">B-</option>
-								<option value="O+">O+</option>
-								<option value="O-">O-</option>
-								<option value="AB+">AB+</option>
-								<option value="AB-">AB-</option>
-								<option value="A1+">A1+</option>
-								<option value="A1-">A1-</option>
-								<option value="A1B+">A1B+</option>
-								<option value="A1B-">A1B-</option>
-								<option value="A2+">A2+</option>
-								<option value="A2-">A2-</option>
-								<option value="A2B+">A2B+</option>
-								<option value="A2B-">A2B-</option>									
-							</select>
-						</div>
-
-						<div class="form-group">
-							<label for="mobileNumber">Mobile Number*</label>
-							<input class="form-control" type="text" name="mobileNumber" for="mobileNumber">
-						</div>
-
-						<div class="form-group">
-							<label for="landLineNumber">Landline Number</label>
-							<input class="form-control" type="text" name="landLineNumber" id="landLineNumber">
-						</div>
-
-						<div class="form-group">
-							<label for="city">City</label>
-							<input class="form-control" type="text" name="city" id="city">
-						</div>
-						<div class="form-group">
-							<label for="address">Address</label>
-							<textarea class="form-control" id="address" rows="4"></textarea>
-						</div>
-						<div class="form-group">
-							<label for="donorEmail">Email</label>
-							<input type="email" name="donorEmail" id="donorEmail" class="form-control">
-						</div>
-						<div class="form-group">
-							<label for="userName">Username</label>
-							<input type="text" name="userName" class="form-control" id="userName">
-						</div>
-
-						<div class="form-group">
-							<label for="password">Password</label>
-							<input type="password" name="password" id="password" class="form-control">
-						</div>
-
-						<div class="form-group">
-							<label for="rePassword">Retype- Password</label>
-							<input type="password" name="rePassword" class="form-control" id="rePassword">
-						</div>
-
-						<div class="form-group">
-							<label for="availability"> Please confirm your availability to donate blood </label>
-							<select id="availability" class="form-control" name="availability">
-								<option> Available </option>
-								<option> Not Available </option>
-							</select>
-						</div>
-						<div class="form-group">
-							<label for="profilePicture">Upload Profile Picture</label>
-							<input type="file" name="donorImage" id="profilePicture">
-						</div>
-						
-						<button type="submit" class="btn btn-danger">Submit</button>
-					</form>
+	<div class="row">
+		<div class="col-sm-8 offset-sm-2 col-md-6 offset-md-3 ">
+			<div class="donorForm">
+				<div class="submit-form text-center">
+					<h4>Blood Donors Register</h4>
+					<p>
+						Please fill the following information to register donor.
+					</p>
+					<hr>
 				</div>
-				
+				<form action="donorregister.php" method="POST" enctype="multipart/form-data">
+					<div class="form-group">
+						<label for="fullName">Full name*</label>
+						<input type="text" class="form-control" id="fullName" aria-describedby="fullName" name="name" placeholder="Enter full name">
+					</div>
+
+					<div class="form-group">
+						<label for="bloodGroup">Blood Group*</label>
+						<select class="form-control" id="bloodGroup" name="bloodgroup">
+							<option selected="selected" disabled="disabled">-----Select-----</option>
+							<option value="A+">A+</option>
+							<option value="A-">A-</option>
+							<option value="B+">B+</option>
+							<option value="B-">B-</option>
+							<option value="O+">O+</option>
+							<option value="O-">O-</option>
+							<option value="AB+">AB+</option>
+							<option value="AB-">AB-</option>
+							<option value="A1+">A1+</option>
+							<option value="A1-">A1-</option>
+							<option value="A1B+">A1B+</option>
+							<option value="A1B-">A1B-</option>
+							<option value="A2+">A2+</option>
+							<option value="A2-">A2-</option>
+							<option value="A2B+">A2B+</option>
+							<option value="A2B-">A2B-</option>									
+						</select>
+					</div>
+
+					<div class="form-group">
+						<label for="mobileNumber">Mobile Number*</label>
+						<input class="form-control" type="text" name="contact" for="mobileNumber">
+					</div>
+
+					<div class="form-group">
+						<label for="landLineNumber">Landline Number</label>
+						<input class="form-control" type="text" name="landlinecontact" id="landLineNumber">
+					</div>
+
+					<div class="form-group">
+						<label for="city">City</label>
+						<input class="form-control" type="text" name="city" id="city">
+					</div>
+					<div class="form-group">
+						<label for="address">Address</label>
+						<textarea class="form-control" id="address" rows="4" name="address"></textarea>
+					</div>
+					<div class="form-group">
+						<label for="donorEmail">Email</label>
+						<input type="email"  id="donorEmail" class="form-control" name="email">
+					</div>
+					<div class="form-group">
+						<label for="availability"> Please confirm your availability to donate blood </label>
+						<select id="availability" class="form-control" name="availability">
+							<option> Available </option>
+							<option> Not Available </option>
+						</select>
+					</div>
+					<div class="form-group">
+						<label for="profilePicture">Upload Profile Picture</label>
+						<input type="file" name="image" id="profilePicture">
+					</div>
+
+					<button type="submit" class="btn btn-danger" name="submit">Submit</button>
+				</form>
 			</div>
+
 		</div>
-		
+	</div>
+
 	
 </div>
 
 <?php
-	include('footer.php');
+include('footer.php');
 ?>
