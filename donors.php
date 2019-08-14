f2881c9b5dbacd93c2e390219fdb8768c4d4a746
manizkou <alertmanishrana@gmail.com>
2019-08-14 11:13:51 +0545

    changes donors

--- donors.php
+++ donors.php
@@ -1,7 +1,91 @@
 <?php
-	include 'header.php';
+include 'header.php';
 ?>
 
+<div class="container">
+	<div class="row donors">
+
+		<div class="col-sm-12">
+			<h2 class="text-center "><u> Donors </u></h2>	
+		</div>
+
+		<div class="col-sm-6 col-md-4">
+			<div class="card">
+				<img class="card-img-top" src="img/blooddonation.jpg" alt="">
+				<div class="card-body">
+					<p><i class="fas fa-user-alt"></i> &nbsp; Harry Potter </h5>
+					<p><i class="fas fa-tint red"></i> &nbsp; O+ </p>
+					<p><i class="fas fa-user-circle"></i> &nbsp; Availability</p>
+					<p><i class="fas fa-location-arrow"> &nbsp; </i>Butwal-13, Belbas</p>
+					<p><i class="fas fa-mobile-alt"></i> &nbsp; 9847451077</p>
+				</div>
+			</div>
+		</div>	
+		<div class="col-sm-6 col-md-4">
+			<div class="card">
+				<img class="card-img-top" src="img/blooddonation.jpg" alt="">
+				<div class="card-body">
+					<p><i class="fas fa-user-alt"></i> &nbsp; Harry Potter </h5>
+					<p><i class="fas fa-tint red"></i> &nbsp; O+ </p>
+					<p><i class="fas fa-user-circle"></i> &nbsp; Availability</p>
+					<p><i class="fas fa-location-arrow"> &nbsp; </i>Butwal-13, Belbas</p>
+					<p><i class="fas fa-mobile-alt"></i> &nbsp; 9847451077</p>
+				</div>
+			</div>
+		</div>	
+		<div class="col-sm-6 col-md-4">
+			<div class="card">
+				<img class="card-img-top" src="img/blooddonation.jpg" alt="">
+				<div class="card-body">
+					<p><i class="fas fa-user-alt"></i> &nbsp; Harry Potter </h5>
+					<p><i class="fas fa-tint red"></i> &nbsp; O+ </p>
+					<p><i class="fas fa-user-circle"></i> &nbsp; Availability</p>
+					<p><i class="fas fa-location-arrow"> &nbsp; </i>Butwal-13, Belbas</p>
+					<p><i class="fas fa-mobile-alt"></i> &nbsp; 9847451077</p>
+				</div>
+			</div>
+		</div>	
+		<div class="col-sm-6 col-md-4">
+			<div class="card">
+				<img class="card-img-top" src="img/blooddonation.jpg" alt="">
+				<div class="card-body">
+					<p><i class="fas fa-user-alt"></i> &nbsp; Harry Potter </h5>
+					<p><i class="fas fa-tint red"></i> &nbsp; O+ </p>
+					<p><i class="fas fa-user-circle"></i> &nbsp; Availability</p>
+					<p><i class="fas fa-location-arrow"> &nbsp; </i>Butwal-13, Belbas</p>
+					<p><i class="fas fa-mobile-alt"></i> &nbsp; 9847451077</p>
+				</div>
+			</div>
+		</div>	
+		<div class="col-sm-6 col-md-4">
+			<div class="card">
+				<img class="card-img-top" src="img/blooddonation.jpg" alt="">
+				<div class="card-body">
+					<p><i class="fas fa-user-alt"></i> &nbsp; Harry Potter </h5>
+					<p><i class="fas fa-tint red"></i> &nbsp; O+ </p>
+					<p><i class="fas fa-user-circle"></i> &nbsp; Availability</p>
+					<p><i class="fas fa-location-arrow"> &nbsp; </i>Butwal-13, Belbas</p>
+					<p><i class="fas fa-mobile-alt"></i> &nbsp; 9847451077</p>
+				</div>
+			</div>
+		</div>	
+		<div class="col-sm-6 col-md-4">
+			<div class="card">
+				<img class="card-img-top" src="img/blooddonation.jpg" alt="">
+				<div class="card-body">
+					<p><i class="fas fa-user-alt"></i> &nbsp; Harry Potter </h5>
+					<p><i class="fas fa-tint red"></i> &nbsp; O+ </p>
+					<p><i class="fas fa-user-circle"></i> &nbsp; Availability</p>
+					<p><i class="fas fa-location-arrow"> &nbsp; </i>Butwal-13, Belbas</p>
+					<p><i class="fas fa-mobile-alt"></i> &nbsp; 9847451077</p>
+				</div>
+			</div>
+		</div>	
+		
+
+
+	</div>
+</div>
 <?php
 	include 'footer.php';
 ?>
