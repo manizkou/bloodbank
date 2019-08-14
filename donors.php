66ce371a3c0096af73f248265587b9a5a660fb19
manizkou <alertmanishrana@gmail.com>
2019-08-14 13:44:36 +0545

    donors changed

--- donors.php
+++ donors.php
@@ -1,4 +1,19 @@
 <?php
+include('classes/application_top.php');
+$a=$_SESSION['admin'];
+$plist=$obj->getDonar();
+if(!empty($_REQUEST['donorid'])){
+	$id=$_REQUEST['donorid'];
+	
+	$pg=$_REQUEST['pg'];
+	$del=$obj->del($id,$pg,DONORREGISTER,'','');
+	
+}
+?>
+
+
+
+<?php
 include 'header.php';
 ?>
 
@@ -9,80 +24,57 @@ include 'header.php';
 			<h2 class="text-center "><u> Donors </u></h2>	
 		</div>
 
-		<div class="col-sm-6 col-md-4">
-			<div class="card">
-				<img class="card-img-top" src="img/blooddonation.jpg" alt="">
-				<div class="card-body">
-					<p><i class="fas fa-user-alt"></i> &nbsp; Harry Potter </h5>
-					<p><i class="fas fa-tint red"></i> &nbsp; O+ </p>
-					<p><i class="fas fa-user-circle"></i> &nbsp; Availability</p>
-					<p><i class="fas fa-location-arrow"> &nbsp; </i>Butwal-13, Belbas</p>
-					<p><i class="fas fa-mobile-alt"></i> &nbsp; 9847451077</p>
+		<div class="col-sm-12">
+			<h1>Donor Filter:</h1>
+			<form action="#" method="" class="form-inline">
+				<div class="form-group">
+					<!-- <label for="bloodGroup"> Blood Group</label> -->
+					<select class="form-control" id="bloodGroup" name="bloodgroup">
+						<option selected="selected" disabled="disabled">
+							-----Select Blood Group -----
+						</option>
+						<option value="A+">A+</option>
+						<option value="A-">A-</option>
+						<option value="B+">B+</option>
+						<option value="B-">B-</option>
+						<option value="O+">O+</option>
+						<option value="O-">O-</option>
+						<option value="AB+">AB+</option>
+						<option value="AB-">AB-</option>
+						<option value="A1+">A1+</option>
+						<option value="A1-">A1-</option>
+						<option value="A1B+">A1B+</option>
+						<option value="A1B-">A1B-</option>
+						<option value="A2+">A2+</option>
+						<option value="A2-">A2-</option>
+						<option value="A2B+">A2B+</option>
+						<option value="A2B-">A2B-</option>									
+					</select>
 				</div>
-			</div>
-		</div>	
-		<div class="col-sm-6 col-md-4">
-			<div class="card">
-				<img class="card-img-top" src="img/blooddonation.jpg" alt="">
-				<div class="card-body">
-					<p><i class="fas fa-user-alt"></i> &nbsp; Harry Potter </h5>
-					<p><i class="fas fa-tint red"></i> &nbsp; O+ </p>
-					<p><i class="fas fa-user-circle"></i> &nbsp; Availability</p>
-					<p><i class="fas fa-location-arrow"> &nbsp; </i>Butwal-13, Belbas</p>
-					<p><i class="fas fa-mobile-alt"></i> &nbsp; 9847451077</p>
-				</div>
-			</div>
-		</div>	
-		<div class="col-sm-6 col-md-4">
-			<div class="card">
-				<img class="card-img-top" src="img/blooddonation.jpg" alt="">
-				<div class="card-body">
-					<p><i class="fas fa-user-alt"></i> &nbsp; Harry Potter </h5>
-					<p><i class="fas fa-tint red"></i> &nbsp; O+ </p>
-					<p><i class="fas fa-user-circle"></i> &nbsp; Availability</p>
-					<p><i class="fas fa-location-arrow"> &nbsp; </i>Butwal-13, Belbas</p>
-					<p><i class="fas fa-mobile-alt"></i> &nbsp; 9847451077</p>
-				</div>
-			</div>
-		</div>	
-		<div class="col-sm-6 col-md-4">
-			<div class="card">
-				<img class="card-img-top" src="img/blooddonation.jpg" alt="">
-				<div class="card-body">
-					<p><i class="fas fa-user-alt"></i> &nbsp; Harry Potter </h5>
-					<p><i class="fas fa-tint red"></i> &nbsp; O+ </p>
-					<p><i class="fas fa-user-circle"></i> &nbsp; Availability</p>
-					<p><i class="fas fa-location-arrow"> &nbsp; </i>Butwal-13, Belbas</p>
-					<p><i class="fas fa-mobile-alt"></i> &nbsp; 9847451077</p>
-				</div>
-			</div>
-		</div>	
-		<div class="col-sm-6 col-md-4">
-			<div class="card">
-				<img class="card-img-top" src="img/blooddonation.jpg" alt="">
-				<div class="card-body">
-					<p><i class="fas fa-user-alt"></i> &nbsp; Harry Potter </h5>
-					<p><i class="fas fa-tint red"></i> &nbsp; O+ </p>
-					<p><i class="fas fa-user-circle"></i> &nbsp; Availability</p>
-					<p><i class="fas fa-location-arrow"> &nbsp; </i>Butwal-13, Belbas</p>
-					<p><i class="fas fa-mobile-alt"></i> &nbsp; 9847451077</p>
-				</div>
-			</div>
-		</div>	
-		<div class="col-sm-6 col-md-4">
-			<div class="card">
-				<img class="card-img-top" src="img/blooddonation.jpg" alt="">
-				<div class="card-body">
-					<p><i class="fas fa-user-alt"></i> &nbsp; Harry Potter </h5>
-					<p><i class="fas fa-tint red"></i> &nbsp; O+ </p>
-					<p><i class="fas fa-user-circle"></i> &nbsp; Availability</p>
-					<p><i class="fas fa-location-arrow"> &nbsp; </i>Butwal-13, Belbas</p>
-					<p><i class="fas fa-mobile-alt"></i> &nbsp; 9847451077</p>
-				</div>
-			</div>
-		</div>	
-		
+				<br>
+				<button type="submit" class="btn btn-danger mx-2"> Search Donor</button>
+			</form>
+		</div>
 
+		<?php
+			$i=1;
+			while($data=$plist->fetch_assoc()){
+		?>
+		<div class="col-sm-6 col-md-4">
+			<div class="card">
+				<img class="card-img-top" src="img/blooddonation.jpg" alt="">
+				<div class="card-body">
+					<p><i class="fas fa-user-alt"></i> &nbsp; <?php echo $data['name']; ?> </p>
+					<p><i class="fas fa-tint red"></i> &nbsp; <?php echo $data['bloodgroup']; ?> </p>
+					<p><i class="fas fa-user-circle"></i> &nbsp; <?php echo $data['availability']; ?> </p>
+					<p><i class="fas fa-location-arrow"> &nbsp; </i> <?php echo $data['address']; ?> </p>
+					<p><i class="fas fa-mobile-alt"></i> &nbsp; <?php echo $data['contact']; ?> </p>
+				</div>
+			</div>
+		</div>	
+		<?php
+			}
+		?>
 
 	</div>
 </div>
